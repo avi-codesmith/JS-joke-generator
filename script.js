6f52b4d58c16d84e5823d2776654e828de7cb70a
avi-codesmith <avi.codesmith@gmail.com>
2025-03-13 19:56:24 +0530

    Implement the loader

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+"use strict";
+
 const container = document.querySelector(".container");
 const btn = document.querySelector("button");
 const jokeH3 = document.querySelector("h3");
 const sound = new Audio("errorSound.mp3");
 const img = document.querySelector("img");
+const loader = document.createElement("div");
 const url =
-  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,political,racist,sexist,explicit";
+
+loader.classList.add("loader");
 
 const getJoke = () => {
+  jokeH3.appendChild(loader);
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
       jokeH3.textContent = `${item.joke}`;
+      loader.style.display = "flex";
+
       if (item.joke === undefined) {
+        loader.style.display = "none";
         img.src = "warning.png";
-        jokeH3.textContent = "Can't generate joke pls try again!";
+        jokeH3.textContent = "⚠️ Can't generate joke pls try again";
         jokeH3.style.color = "orangered";
         jokeH3.classList.add("move");
         sound.play();
@@ -24,10 +33,18 @@ const getJoke = () => {
         jokeH3.style.color = "#333";
         img.src = "smile.png";
       }
+      if (img.src.includes("smile.png")) {
+        img.classList.add("rotate");
+      } else if (img.src.includes("warning.png")) {
+        img.classList.remove("rotate");
+      }
     });
+
+  getJoke.onload(() => {
+    loader.style.display = "none";
+  });
 };
 
-window.addEventListener("load", getJoke);
 btn.addEventListener("click", getJoke);
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
